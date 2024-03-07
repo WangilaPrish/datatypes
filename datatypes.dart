1908eacc1fb81bc52be2a29d013172becf60e924
WangilaPrish <nabwonyaprish@gmail.com>
2024-03-07 10:28:46 +0300

    data types

---
+++ datatypes.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Integer data type for age
+  int age = 20;
+
+  // Double data type for GPA
+  double gpa = 3.8;
+
+  // String data type for student name
+  String name = 'Alice Smith';
+
+  // List data type for courses
+  List<String> courses = ['Mathematics', 'Computer Science', 'Physics'];
+
+  // Map data type for student details
+  Map<String, dynamic> student = {
+    'name': 'Priscilla Wangila',
+    'age': 20,
+    'major': 'Earth and Climate Science',
+    'gpa': 3.8,
+    'courses': ['Mathematics', 'Meteorology', 'Physics']
+  };
+
+  // Printing values to demonstrate the use of data types
+  print('Student Name: $name');
+  print('Student Age: $age');
+  print('Student GPA: $gpa');
+  print('Student Courses: $courses');
+  print('Student Details: $student');}
